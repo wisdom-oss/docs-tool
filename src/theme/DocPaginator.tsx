70056aa1a50db2ee187dab85998fa518dda85919
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-01-17 12:57:02 +0100

    documented swizzled classes

--- src/theme/DocPaginator.tsx
+++ src/theme/DocPaginator.tsx
@@ -1,6 +1,10 @@
-import OriginalDocPaginator from "@theme-original/DocPaginator";
 import React from "react";
 
+/**
+ * Deactivated DocPaginator, for our purpose the navigator is not practical.
+ * @param props ignored props but necessary for react
+ * @constructor
+ */
 export default function DocPaginator(props): JSX.Element {
   return (
     <>
--- src/theme/NavbarItem/DropdownNavbarItem.tsx
+++ src/theme/NavbarItem/DropdownNavbarItem.tsx
@@ -2,18 +2,34 @@ import React from 'react';
 import OriginalDropdownNavbarItem from "@theme-original/NavbarItem/DropdownNavbarItem";
 import {useLocation} from "react-router-dom";
 
+/** Icon in the left of the label */
 const labelIcon = "🪴"; // for now it's a potted plant
 
+/**
+ * Overloaded DropdownNavbarItem to allow for another feature without the use
+ * of a custom plugin.
+ *
+ * This allows to get a dropdown menu for branch selection.
+ * @param props Props for react
+ * @constructor
+ */
 function DropdownNavbarItem({...props}): JSX.Element {
   if (props.customType == "branchSelect") {
+    // now we now that we want a branch selector
     try {
+      // extract the current repo and branch from the location
       let [match, currentRepo, currentBranch] = useLocation().pathname
         .match(/\/repos\/([^\/]+)\/([^\/]+)/);
+
       props.label = labelIcon + "Branch - " + currentBranch;
       props.items = [];
       for (let branch of props.branches[currentRepo]) {
+        // iterate over every branch of the current repo
+
+        // use the current location with a selected branch
         let currentLocation = useLocation().pathname
           .replace(/\/repos\/([^\/]+)\/[^\/]+/, "/repos/$1/" + branch);
+
         props.items.push({
           to: currentLocation,
           target: "_self",
@@ -22,6 +38,8 @@ function DropdownNavbarItem({...props}): JSX.Element {
       }
     }
     catch (e) {
+      // on some pages the dropdown cannot find the current branch
+      // but this is not a big issue, will simply be not displayed
       console.warn(
         "The branch dropdown cannot find repo or branch in " +
         useLocation().pathname
@@ -29,10 +47,13 @@ function DropdownNavbarItem({...props}): JSX.Element {
       return null;
     }
     finally {
+      // remove the data from the html tag, since it's not necessary to show
+      // there
       delete props.branches;
       delete props.customType;
     }
   }
+
   return <OriginalDropdownNavbarItem {...props}/>
 }
 
